9afc70cff45ae03105c222de2f1753d7218b9ac8
balogunibrahim <balooopy30@gmail.com>
2025-11-15 02:15:08 +0000

    coding an encoder and a decoder for tekoenization

--- llm.py
+++ llm.py
@@ -53,3 +53,48 @@ vocab_size = len(all_words)
 
 # Print the size of the vocabulary (number of unique words)
 print(vocab_size)
+
+# Create a vocabulary dictionary where:
+#   - each UNIQUE token (word) is the key
+#   - each token gets assigned a unique integer ID
+# enumerate(all_words) gives pairs like: (0, 'a'), (1, 'apple'), (2, 'book'), ...
+# The dictionary comprehension flips it to: {'a': 0, 'apple': 1, 'book': 2, ...}
+vocab = {token: integer for integer, token in enumerate(all_words) }
+
+# --- SIMPLE TOKENIZER CLASS ---
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        # str_to_int: maps words to integers
+        self.str_to_int = vocab
+        # int_to_str: reverse mapping (integer → word)
+        # Needed for decoding
+        self.int_to_str = {i:s for s, i in vocab.items()}
+    
+
+    def encode(self, text):
+        # Split the input text into tokens using a regex pattern.
+        # This pattern separates words from punctuation and whitespace.
+        preprocessed = re.split(r'([,.:;"()\']|--|\s)', text)
+        
+        # Remove empty strings and stray spaces from the token list.
+        # `strip()` cleans each token, and the condition filters out empty items.
+        preprocessed = [ item.strip() for item in preprocessed if item.strip()]
+
+        # Convert each cleaned token into its corresponding integer ID
+        # using the vocabulary mapping (string → integer).
+        ids = [self.str_to_int[s] for s in preprocessed]
+
+        # Return the list of integer token IDs.
+        return ids
+    
+    def decoder(self, ids):
+        # Convert each integer ID back into its corresponding token (word/punctuation)
+        # Then join all tokens into a single string separated by spaces.
+        text = " ".join([self.int_to_str[i] for i in ids])
+         
+         # Fix spacing before punctuation.
+        # The regex finds spaces before punctuation marks and removes them.
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        
+        # Return the cleaned, human-readable text.
+        return text
